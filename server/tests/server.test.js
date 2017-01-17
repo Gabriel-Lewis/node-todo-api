cec87be89c8d9ae7bb9a5b4641af9c5193d6a33b
Gabriel Lewis <benbowgl1@gmail.com>
2017-01-17 15:03:35 -0800

    Adds tests for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -221,7 +221,7 @@ describe("POST /users", () => {
           expect(user).toExist();
           expect(user.password).toNotBe(password);
           done();
-        });
+        }).catch((e) => (done(e)));;
       });
   });
 
@@ -255,7 +255,58 @@ describe("POST /users", () => {
       })
       .end(done)
   });
+});
 
+describe("POST /users/login", () => {
+  it("should login user and return auth token", (done) => {
+    let email = users[1].email;
+    let password = users[1].password;
 
+    request(app)
+      .post('/users/login')
+      .send({email, password})
+      .expect(200)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toExist();
+        expect(res.body._id).toExist();
+        expect(res.body.email).toBe(email);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err)
+        }
 
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens[0]).toInclude({
+            access: 'auth',
+            token: res.headers['x-auth']
+          })
+          done();
+        }).catch((e) => (done(e)));
+      })
+
+  });
+
+  it("should reject invalid login", (done) => {
+    let email = users[1].email;
+    let password = 'thiasdkfj';
+
+    request(app)
+      .post('/users/login')
+      .send({email, password})
+      .expect(400)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toNotExist();
+      })
+      .end((err, res) => {
+        if (err) {
+          done(err)
+        }
+
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens.length).toBe(0)
+          done();
+      }).catch((e) => done(e));
+    });
+  });
 });
